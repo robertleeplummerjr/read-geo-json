dc5938d39795b8bf6b7be9f3c47f0473214a0c60
Robert Plummer <robertleeplummerjr@gmail.com>
2016-01-14 15:31:59 -0500

    main module

---
+++ index.js
@@ -0,0 +1,52 @@
+var defaults = {
+  eachPoint: null,
+  eachLineString: null,
+  eachPolygon: null,
+  eachMultiPoint: null,
+  eachMultiLineString: null,
+  eachMultiPolygon: null,
+  eachGeometryCollection: null
+};
+
+function readGeoJSON(geoJson, settings) {
+  if (geoJson === null) throw new Error('geoJson property not defined');
+
+  for (var i in defaults) {
+    if (defaults.hasOwnProperty(i)) {
+      settings[i] = settings.hasOwnProperty(i) ? settings[i] : defaults[i]
+    }
+  }
+
+  if (geoJson.type === 'FeatureCollection') {
+    if (geoJson.features instanceof Array) {
+      geoJson.features.forEach(function(feature) {
+        eachFeature(feature, geoJson.features);
+      });
+    }
+  } else {
+    eachFeature(geoJson);
+  }
+
+  function eachFeature(feature, featureCollection) {
+    if (feature.hasOwnProperty('geometry')) {
+      eachGeometry(feature.geometry, feature, featureCollection);
+    } else if (feature.hasOwnProperty('geometries') && feature.geometries instanceof Array) {
+      feature.geometries.forEach(function(geometry) {
+        eachGeometry(geometry, feature, featureCollection);
+      });
+    }
+  }
+
+  function eachGeometry(geometry, feature, featureCollection) {
+    var fn = settings['each' + geometry.type];
+
+    switch (typeof fn) {
+      case 'function': fn(geometry, feature, featureCollection); break;
+      case 'undefined': throw new Error('unknown type: ' + (geometry.type || 'empty')); break;
+      default:
+        console.log('unhandled type: ' + geometry.type);
+    }
+  }
+}
+
+module.exports = readGeoJSON;
